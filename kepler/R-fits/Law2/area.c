2608db2c539a43ee0419624140a06844fb3bf816
oscarsaleta <oscar.saleta@gmail.com>
2015-06-29 15:13:16 +0200

    more kepler

--- kepler/R-fits/Law2/area.c
+++ kepler/R-fits/Law2/area.c
@@ -1,3 +1,5 @@
+/* gcc -Wall -o area area.c -lm */
+
 #include <stdio.h>
 #include <math.h>
 
@@ -5,8 +7,9 @@ int main (int argc, char *argv[]) {
     char nomFitxer[30];
     double focus;
     double x[2],y[2],t[2];
-    double theta[2],dtheta,r;
-    double foo;
+    double theta[2],dtheta,r[2];
+    double theta_f[2],dtheta_f,r_f;
+    double energy,darea;
     FILE *fitxer;
 
     //Lectura paràmetres
@@ -25,19 +28,27 @@ int main (int argc, char *argv[]) {
         return 1;
     }
 
-    fprintf(stdout,"#x y t dA/dt\n");
+    fprintf(stdout,"#x y t dA/dt K\n");
 
-    fscanf(fitxer,"%lf %lf %lf %lf",&x[0],&y[0],&t[0],&foo);
-    theta[0] = atan(y[0]/x[0]);
+    fscanf(fitxer,"%lf %lf %lf %lf",&x[0],&y[0],&t[0],&r[0]);
+    theta[0] = atan2(y[0],x[0]);
+    theta_f[0] = atan2(y[0],x[0]+fabs(focus));
     while (!feof(fitxer)) {
-        fscanf(fitxer,"%lf %lf %lf %lf",&x[1],&y[1],&t[1],&foo);
-        r = sqrt( (x[0]-focus)*(x[0]-focus)+y[0]*y[0] );
-        theta[1] = atan(y[1]/x[1]);
+        fscanf(fitxer,"%lf %lf %lf %lf",&x[1],&y[1],&t[1],&r[1]);
+        r_f = sqrt( (x[0]-focus)*(x[0]-focus)+y[0]*y[0] );
+        theta[1] = atan2(y[1],x[1]);
         dtheta = (theta[1]-theta[0])/(t[1]-t[0]);
-        fprintf(stdout,"%10.6G %10.6G %10.6G %10.6G\n",x[1],y[1],t[1],0.5*r*r*dtheta);
+        theta_f[1] = atan2(y[1],x[1]+fabs(focus));
+        dtheta_f = (theta_f[1]-theta_f[0])/(t[1]-t[0]);
+        darea = 0.5*r_f*r_f*dtheta_f;
+        energy = 0.5*r[0]*r[0]*dtheta*dtheta-0.5/(r[0]*r[0]);
+        fprintf(stdout,"%10.6G %10.6G %10.6G %10.6G %10.6G\n",x[1],y[1],t[1],fabs(darea),energy);
         x[0]=x[1];
         y[0]=y[1];
+        t[0]=t[1];
+        r[0]=r[1];
         theta[0]=theta[1];
+        theta_f[0]=theta_f[1];
     }
 
     fclose(fitxer);
---
+++ kepler/R-fits/Law2/plot.plt
@@ -0,0 +1,30 @@
+reset
+set encoding utf8
+set terminal postscript eps color enhanced
+set output 'kepler.eps'
+
+
+set grid lc rgb "#808080" lt 0 lw 1
+
+set xrange [0:2]
+set xlabel 't'
+
+set multiplot layout 1,2 margins 0.08,0.98,0.1,0.9 spacing 0.08,0.15
+
+set yrange [:0.517]
+set title 'Area sweeped per unit time'
+plot 'tr10-area.dat' u 3:($4<1 ? $4 : 1/0) w l title 'r_0',\
+     'tr050-area.dat' u 3:($4<1 ? $4 : 1/0) w l title 'r_1',\
+     'tr20-area.dat' u 3:($4<1 ? $4 : 1/0) w l title 'r_2'
+
+set yrange[:0.001]
+unset key
+set title 'Energy'
+plot 'tr10-area.dat' u 3:($5<0 ? $5 : 1/0) w l title 'r_0',\
+     'tr050-area.dat' u 3:($5<0 ? $5 : 1/0) w l title 'r_1',\
+     'tr20-area.dat' u 3:($5<0 ? $5 : 1/0) w l title 'r_2'
+
+
+unset multiplot
+
+system('epstopdf kepler.eps && rm *.eps')
